951a8c96071cd1df25cce156c94fdc348e2ae975
Farhan <101630148+KucingOyenBarbar@users.noreply.github.com>
2025-03-08 18:04:19 +0700

    feat add common response

--- constants/error/general.go
+++ constants/error/general.go
@@ -2,18 +2,13 @@ package error
 
 import "errors"
 
-const (
-	Success = "success"
-	Error   = "error"
-)
-
 var (
-	ErrInternalServerError = errors.New("Internal Server Error")
-	ErrSQLError            = errors.New("Database server failed to execute query")
-	ErrToManyRequests      = errors.New("Too many requests")
+	ErrInternalServerError = errors.New("internal server error")
+	ErrSQLError            = errors.New("database server failed to execute query")
+	ErrToManyRequests      = errors.New("too many requests")
 	ErrUnauthorized        = errors.New("unauthorized")
-	ErrInvalidToken        = errors.New("Invalid token")
-	ErrForbidden           = errors.New("Forbidden")
+	ErrInvalidToken        = errors.New("invalid token")
+	ErrForbidden           = errors.New("forbidden")
 )
 
 var GeneralErrors = []error{
