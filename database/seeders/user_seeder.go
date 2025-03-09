589a57c5933c0f076ce42a1b52fc0c629c4f60a3
Farhan <101630148+KucingOyenBarbar@users.noreply.github.com>
2025-03-09 07:45:14 +0700

    feat add database seeders

---
+++ database/seeders/user_seeder.go
@@ -0,0 +1,32 @@
+package seeders
+
+import (
+	"user-service/constants"
+	"user-service/domain/models"
+
+	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
+
+func RunUserSeeder(db *gorm.DB) {
+	password, _ := bcrypt.GenerateFromPassword([]byte("admin123"), bcrypt.DefaultCost)
+	user := models.User{
+		UUID:        uuid.New(),
+		Name:        "Administrator",
+		Username:    "admin",
+		Email:       "admin@gmail.com",
+		Password:    string(password),
+		PhoneNumber: "082298717364",
+		RoleID:      constants.Admin,
+	}
+
+	err := db.FirstOrCreate(&user, models.User{Username: user.Username}).Error
+	if err != nil {
+		logrus.Errorf("failed to seed user: %v", err)
+		panic(err)
+
+	}
+
+}
